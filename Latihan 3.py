35973c72ccd398147b872270b3224b4d7c798554
Ikhsan Fakhrudin <ihsaanef@gmail.com>
2022-10-21 14:06:56 +0700

    File Pendukung

--- Latihan 3.py
+++ Latihan 3.py
@@ -4,3 +4,10 @@ print("variabel A=", a)
 b=input("nilai b?")
 print("variabel B=", b)
 print("penjumlahan A+B=", int(a)+int(b))
+
+r = input("Masukan jari-jari Lingkaran:")
+pi = 3.314
+l = pi * int(r) * int(r)
+k = 2 * pi * int(r)
+print("Luas Lingkaran:", l)
+print("Keliling Lingkaran:" ,k)
